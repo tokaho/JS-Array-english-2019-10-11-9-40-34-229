a10dcd1334c2277290c7e93c6880a9ab076c6e75
tokaho <tokaho2000@gmail.com>
2022-11-21 11:59:00 +0800

    ....

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 // The writer determines whether the following variables are of type array.
 var a = '[a, b, c, d]';
 var b = [1, 2, 3, 4];
-//TODO
 
+//TODO
+typeof(a)=='object'
+typeof(b)=='object'
 
 // Write a program that multiplies each entry in the following array by 2。
 var a = [1, 2, 3, 4, 5];
 // TODO should output [2,4,6,8,10]
+a=a.map((x)=>x*2)
 
 
 // Write the program, according to the following requirements output results.
@@ -14,13 +17,34 @@ var colors = ["Red", "Green", "White", "Black"];
 //TODO case 1 output: 'Red Green White Black'
 // case 2 output: 'Red+Green+White+Black'
 // case 3 output: 'Red,Green,White,Black'
-
+switch(c) {
+    case 1:
+      // code block
+      console.log(colors.join(' '))
+      break;
+    case 2:
+      // code block
+      console.log(colors.join('+'))
+      break;
+    case 3:
+      // code block
+      console.log(colors.join(','))
+  }
 
 // Write a program to sort the Numbers in the following array from largest to smallest.
 var a = [5, 1, 8, 10, 4];
 //TODO should output: [10,8,5,4,1]
-
+a.sort((a, b)=>b - a);
 
 // Program to find the most frequent element in the following array.
 var a = [3, 'a', 'a', 'a', 2, 3, 'a', 3, 'a', 2, 4, 9, 3];
 //TODO should output: 'a'
+temp={}
+for(e in a){
+    if(temp[a[e]] != undefined){
+        temp[a[e]]+=1
+      }else{
+        temp[a[e]]=1
+      }
+}
+Object.keys(temp).reduce((x,y)=>temp[x] > temp[y] ? x : y )
